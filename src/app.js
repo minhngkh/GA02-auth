6bbc67b32976bb42e1f72a6961a81596ae6b89ef
thinhngoo <ncathinh21@clc.fitus.edu.vn>
2023-11-28 15:44:41 +0700

    :wrench: Update register auth to now functional

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const hbs = require("hbs");
 const passport = require("./lib/passport");
@@ -23,7 +22,6 @@ hbs.registerPartials(path.join(__dirname, "views/partials"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
 
 // TODO: Implement CSRF protection
--- src/components/auth/router.js
+++ src/components/auth/router.js
@@ -3,25 +3,33 @@ const router = express.Router();
 
 const passport = require("../../lib/passport");
 
-const { check, validationResult, body } = require("express-validator");
+const db = require("../../db/client");
+const { users } = require("../../db/schema");
+
+const { validationResult, body } = require("express-validator");
 
 router.get("/login", (req, res, _) => {
   res.render("auth/login", {
     title: "Login",
     buttons: [{ name: "Register", route: "/auth/register" }],
   });
-})
+});
 
-router.post("/login",
-  body('username')
-    .not().isEmpty().withMessage('You must input Username')
-    .isLength({ min: 3, max: 10 }).withMessage("Your username's length must in range [3-10]")
-    .matches(/^[a-zA-Z0-9]+$/).withMessage("Your username can contain only Number and Alphabet"),
-  body('password')
-    .not().isEmpty().withMessage("You must input password")
-    .isLength({ min: 6, max: 20 }).withMessage("Your username's length must in range [6-20]"),
+router.post(
+  "/login",
+  body("username")
+    .notEmpty()
+    .withMessage("You must input Username")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Your username's length must in range [3-10]")
+    .matches(/^[a-zA-Z0-9]+$/)
+    .withMessage("Your username can contain only Number and Alphabet"),
+  body("password")
+    .notEmpty()
+    .withMessage("You must input password")
+    .isLength({ min: 6, max: 20 })
+    .withMessage("Your username's length must in range [6-20]"),
   (req, res, next) => {
-    const errors = validationResult(req);
     const err = validationResult(req);
     if (!err.isEmpty()) {
       console.log("Validation error");
@@ -32,61 +40,91 @@ router.post("/login",
     successReturnToOrRedirect: "/test/login1",
     failureRedirect: "/auth/login",
     failureMessage: true,
-  })
-)
+  }),
+);
 
 router.get("/register", (req, res, _) => {
   res.render("auth/register", {
     title: "Register",
     buttons: [{ name: "Login", route: "/auth/login" }],
-  })
-})
+  });
+});
 
 const crypto = require("crypto");
-router.post("/register",
-  body('username')
-    .not().isEmpty().withMessage('You must input Username')
-    .isLength({ min: 3, max: 10 }).withMessage("Your username's length must in range [3-10]")
-    .matches(/^[a-zA-Z0-9]+$/).withMessage("Your username can contain only Number and Alphabet"),
-  body('email')
-    .not().isEmpty().withMessage('You must input Email'),
-  body('password')
-    .not().isEmpty().withMessage("You must input Password")
-    .isLength({ min: 6, max: 20 }).withMessage("Your password's length must in range [6-20]"),
-  body('re-password')
-    .not().isEmpty().withMessage("You must input Retype-password")
-    .custom((value, { req }) => value === req.body.password).withMessage("Password confirmation does not match password"),
+router.post(
+  "/register",
+  [
+    body("username")
+      .notEmpty()
+      .withMessage("You must input Username")
+      .isLength({ min: 3, max: 10 })
+      .withMessage("Your username's length must in range [3-10]")
+      .matches(/^[a-zA-Z0-9]+$/)
+      .withMessage("Your username can contain only Number and Alphabet"),
+    body("email").notEmpty().isEmail().withMessage("You must input Email"),
+    body("password")
+      .notEmpty()
+      .withMessage("You must input Password")
+      .isLength({ min: 6, max: 20 })
+      .withMessage("Your password's length must in range [6-20]"),
+    body("re-password")
+      .not()
+      .isEmpty()
+      .custom((value, { req }) => value === req.body.password)
+      .withMessage("Password confirmation does not match"),
+  ],
   (req, res, next) => {
     // test validator
-
     const err = validationResult(req);
     if (!err.isEmpty()) {
       console.log("Validation error");
-      res.redirect("/auth/register")
+      return res.redirect("/auth/register");
     }
     next();
   },
-  passport.authenticate("signup", {
-    failureRedirect: "/auth/register",
-    failureMessage: true,
-  },
-  (req, res) => {
+  (req, res, next) => {
+    const salt = crypto.randomBytes(16);
     crypto.pbkdf2(
       req.body.password,
-      req.body.username,
+      salt,
       310000,
       32,
       "sha256",
       async (err, hashedPassword) => {
-        if (!crypto.timingSafeEqual(user.hashed_password, hashedPassword)) {
-          await db.insert(users).values({ username: req.body.username, email: req.body.email, hashed_password: hashedPassword, salt: req.body.username  });
-          
+        if (err) {
+          return next(err);
         }
+        const result = await db
+          .insert(users)
+          .values({
+            username: req.body.username,
+            email: req.body.email,
+            hashed_password: hashedPassword,
+            salt: salt,
+          })
+          .returning({
+            insertedId: users.id,
+          })
+          .catch((err) => {
+            return next(err);
+          });
+
+        req.login(
+          {
+            id: result.insertedId,
+            username: req.body.username,
+          },
+          (err) => {
+            if (err) {
+              return next(err);
+            }
+            res.redirect("/protected");
+          },
+        );
       },
     );
-    res.redirect("/test/login1")
-  })
-)
+  },
+);
 
 router.get("/logout", (req, res, _) => {
   res.send("logout page");
